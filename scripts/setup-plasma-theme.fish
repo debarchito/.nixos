be944ff10f6bb64af89f15354c90e2fc00a48977
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-04-03 08:44:43 +0530

    Add script to setup plasma theme

---
+++ scripts/setup-plasma-theme.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+# Setup plasma theme (catppuccin)
+git clone --depth=1 https://github.com/catppuccin/kde ~/.dotfiles/themes/catppuccin-kde
+cd ~/.dotfiles/themes/catppuccin-kde
+bash install.sh
