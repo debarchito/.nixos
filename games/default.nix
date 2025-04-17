04dbb03c706e5303c989f76c1e43145ec5b7d840
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-04-17 17:55:11 +0530

    Add steam and stuff

--- games/default.nix
+++ games/default.nix
@@ -2,7 +2,22 @@
   imports = [
     ./aagl.nix
   ];
-  # Some gaming related stuff that I always want
-  programs.gamescope.enable = true;
+
+  # Some stuff I suppose.
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
   programs.gamemode.enable = true;
+
+  # Steam stuff.
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    gamescopeSession.enable = true;
+  };
+
+  # Controller stuff.
+  hardware.xone.enable = true;
 }
