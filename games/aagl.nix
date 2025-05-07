54c73f2d35306fdedeb646e351c3b3076611065b
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-05-07 16:00:24 +0530

    Remove an-anime-game-launcher

--- games/aagl.nix
+++ games/aagl.nix
@@ -3,6 +3,5 @@
 {
   imports = [ inputs.aagl.nixosModules.default ];
   nix.settings = inputs.aagl.nixConfig;
-  programs.anime-game-launcher.enable = true;
   programs.honkers-railway-launcher.enable = true;
 }
