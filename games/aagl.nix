d13894f42a54abe3b6ebc0b9c6d71b08cd740119
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-05-11 11:46:32 +0530

    Add honkers-launcher for launcher the game

--- games/aagl.nix
+++ games/aagl.nix
@@ -3,5 +3,8 @@
 {
   imports = [ inputs.aagl.nixosModules.default ];
   nix.settings = inputs.aagl.nixConfig;
+  # DO NOT USE THESE TO MANAGE THE GAMES, ONLY LAUNCH!
+  # Use HoyoPlay to manage the games.
+  programs.honkers-launcher.enable = true;
   programs.honkers-railway-launcher.enable = true;
 }
