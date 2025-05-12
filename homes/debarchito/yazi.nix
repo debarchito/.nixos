91528b238778fa7e5d75992f9d140619ffc868bb
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-05-12 11:31:18 +0530

    Move yazi config to nix

--- homes/debarchito/yazi.nix
+++ homes/debarchito/yazi.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   programs.yazi = {
@@ -13,8 +13,80 @@
       yatline-catppuccin = pkgs.yaziPlugins.yatline-catppuccin;
     };
     initLua = ./yazi/init.lua;
+    settings = {
+      manager.ratio = [
+        2
+        2
+        5
+      ];
+      plugin = {
+        prepend_previewers = [
+          {
+            name = "*.csv";
+            run = "duckdb";
+          }
+          {
+            name = "*.tsv";
+            run = "duckdb";
+          }
+          {
+            name = "*.json";
+            run = "duckdb";
+          }
+          {
+            name = "*.parquet";
+            run = "duckdb";
+          }
+          {
+            name = "*.txt";
+            run = "duckdb";
+          }
+          {
+            name = "*.xlsx";
+            run = "duckdb";
+          }
+          {
+            name = "*.db";
+            run = "duckdb";
+          }
+          {
+            name = "*.duckdb";
+            run = "duckdb";
+          }
+        ];
+        prepend_preloaders = [
+          {
+            name = "*.csv";
+            run = "duckdb";
+            multi = false;
+          }
+          {
+            name = "*.tsv";
+            run = "duckdb";
+            multi = false;
+          }
+          {
+            name = "*.json";
+            run = "duckdb";
+            multi = false;
+          }
+          {
+            name = "*.parquet";
+            run = "duckdb";
+            multi = false;
+          }
+          {
+            name = "*.txt";
+            run = "duckdb";
+            multi = false;
+          }
+          {
+            name = "*.xlsx";
+            run = "duckdb";
+            multi = false;
+          }
+        ];
+      };
+    };
   };
-  xdg.configFile."yazi/yazi.toml".source = lib.mkForce ./yazi/yazi.toml;
-  xdg.configFile."yazi/theme.toml".source = lib.mkForce ./yazi/theme.toml;
-  xdg.configFile."yazi/catppuccin-mocha.tmTheme".source = lib.mkForce ./yazi/catppuccin-mocha.tmTheme;
 }
