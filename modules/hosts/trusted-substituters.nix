8229fe9a36d6e79bdebed6c13dcb672e63fa85db
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-05-16 16:00:48 +0000

    Update substituters

--- modules/hosts/trusted-substituters.nix
+++ modules/hosts/trusted-substituters.nix
@@ -6,12 +6,10 @@
   config = lib.mkIf config.trusted-substituters.enable {
     nix.settings = {
       substituters = [
-        "https://ezkea.cachix.org"
         "https://devenv.cachix.org"
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
-        "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
