27b44594484b6d88d119bcd301065fed27711974
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-05-22 15:08:24 +0530

    Pin to NixOS 25.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     catppuccin.url = "github:catppuccin/nix";
     home-manager = {
